00169b86f35aec810920d149c0a2389e73159ad6
Nazarova Elena <ln15ln@rambler.ru>
2016-10-06 14:58:17 +0400

    Set custom exit black without white error border behind

--- GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/AboutFragment.java
+++ GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/AboutFragment.java
@@ -3,22 +3,29 @@ package com.lena.googletranslateexample;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class AboutFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.about_dialog_title)
-                .setMessage(R.string.about_dialog_text)
-                .setPositiveButton(R.string.ok_button_text, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dismiss();
-                    }
-                });
+        StyleAlertDialog adb = new StyleAlertDialog(getActivity(), R.style.AlertDialogCustom);
+        adb.setView(getActivity().getLayoutInflater().inflate(R.layout.fragment_about, null));
+        adb.setTitle("Aboutt");
+        adb.setButton(AlertDialog.BUTTON_POSITIVE, "Okk", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+                //adb.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        return new android.support.v7.app.AlertDialog.Builder(getActivity(), R.style.AlertDialogCustom).show();
+    }
 
-        return builder.create();
+    private class StyleAlertDialog extends AlertDialog {
+        StyleAlertDialog(Context context, int theme) {
+            super(context, theme);
+        }
     }
 }
--- GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/ExitFragment.java
+++ GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/ExitFragment.java
@@ -1,6 +1,5 @@
 package com.lena.googletranslateexample;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
@@ -10,7 +9,7 @@ public class ExitFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(getActivity(), R.style.AlertDialogCustom);
         builder.setTitle(R.string.exit_dialog_title)
                 .setMessage(R.string.exit_dialog_text)
                 .setPositiveButton(R.string.no_button_text, new DialogInterface.OnClickListener() {
@@ -24,6 +23,8 @@ public class ExitFragment extends DialogFragment {
                     }
                 });
 
-        return builder.create();
+        android.support.v7.app.AlertDialog a =  builder.create();
+        a.show();
+        return  a;
     }
 }
