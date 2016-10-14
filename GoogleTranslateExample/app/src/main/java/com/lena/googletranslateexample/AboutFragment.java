3e9202003e268251240b39d622d8ea3f9016244e
Nazarova Elena <ln15ln@rambler.ru>
2016-10-14 10:19:30 +0400

    Change theme and AlertDialogs to support

--- GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/AboutFragment.java
+++ GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/AboutFragment.java
@@ -1,6 +1,6 @@
 package com.lena.googletranslateexample;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
@@ -9,7 +9,7 @@ import android.os.Bundle;
 public class AboutFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AlertDialogCustom);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());//, R.style.AlertDialogCustom);
         builder.setTitle(R.string.about_dialog_title)
                 .setMessage(R.string.about_dialog_text)
                 .setNeutralButton(R.string.ok_button_text, new DialogInterface.OnClickListener() {
--- GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/ExitFragment.java
+++ GoogleTranslateExample/app/src/main/java/com/lena/googletranslateexample/ExitFragment.java
@@ -1,6 +1,6 @@
 package com.lena.googletranslateexample;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
@@ -9,7 +9,7 @@ import android.os.Bundle;
 public class ExitFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-       AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AlertDialogCustom);
+       AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());//, R.style.AlertDialogCustom);
         builder.setTitle(R.string.exit_dialog_title)
                 .setMessage(R.string.exit_dialog_text)
                 .setPositiveButton(R.string.no_button_text, new DialogInterface.OnClickListener() {
